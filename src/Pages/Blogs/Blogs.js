803e13790692068a2cdc416661fe9abcd1d36fc5
nusrat78 <njahan5061@gmail.com>
2022-05-24 22:48:38 +0600

    purchase route set up

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,9 +2,111 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            this is Blogs
-        </div>
+        <section className='w-auto'>
+            <div className='mt-5'>
+                <label for="my-modal-6" class="btn modal-button">1. How will you improve the performance of a React Application?
+
+                </label>
+
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal-6" class="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5'>
+                <label for="my-modal-6" class="btn modal-button">2. What are the different ways to manage a state in a React application?
+
+                </label>
+
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal-6" class="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5'>
+                <label for="my-modal-6" class="btn modal-button">3. How does prototypical inheritance work?
+
+                </label>
+
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal-6" class="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5'>
+                <label for="my-modal-6" class="btn modal-button">4.Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts
+
+                </label>
+
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal-6" class="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5'>
+                <label for="my-modal-6" class="btn modal-button">5.You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?
+
+
+
+                </label>
+
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal-6" class="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5'>
+                <label for="my-modal-6" class="btn modal-button">6.What is a unit test? Why should write unit tests?
+
+                </label>
+
+                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <div class="modal-action">
+                            <label for="my-modal-6" class="btn">Yay!</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section >
+
+
+
+
+
     );
 };
 
--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -6,7 +6,7 @@ const Purchase = () => {
     const [tool, setTool] = useState({});
 
     useEffect(() => {
-        const url = `tools.json/${toolId}`;
+        const url = `http://localhost:5000/tools/${toolId}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setTool(data));
@@ -14,11 +14,19 @@ const Purchase = () => {
 
 
     return (
-        <div>
-            <img src={tool.images} alt=""></img>
-            <h2>{tool.name}</h2>
+        <div >
+            <div class="card  lg:max-w-lg mx-auto bg-base-100 shadow-xl">
+                <figure class="px-10 pt-10">
+                    <img src={tool.images} alt="" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">{tool.name}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
 
+                </div>
+            </div>
         </div>
+
     );
 };
 
